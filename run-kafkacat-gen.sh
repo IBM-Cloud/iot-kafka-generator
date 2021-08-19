367a70fc7dda0499c63548c9b0a8060740b37ca3
torsten <torsten@de.ibm.com>
2021-08-19 11:40:00 +0200

    Print message_timestamp when sending to stdout

--- run-kafkacat-gen.sh
+++ run-kafkacat-gen.sh
@@ -2,8 +2,9 @@ if [ -z "$KAFKA_BROKERS_SASL" ]; then echo "Error: environment variable KAFKA_BR
 if [ -z "$PASSWORD" ]; then echo "Error: environment variable PASSWORD is not set." && exit 1; fi
 if [ -z "$TOPIC_NAME" ]; then echo "Error: environment variable TOPIC_NAME is not set." && exit 1; fi
 
-awk -v timestamp="`date +"%Y-%m-%d %H:%M:%S"`" '{sub("CURRENT TIMESTAMP", timestamp)}1' sample-payload-iotmessages.json > payload.temp
+timestamp=`date +"%Y-%m-%d %H:%M:%S"`
+awk -v timestamp="$timestamp" '{sub("CURRENT TIMESTAMP", timestamp)}1' sample-payload-iotmessages.json > payload.temp
 
-echo Sending IOT Messages to topic $TOPIC_NAME
+echo Sending IOT Messages with message_timestamp=\"$timestamp\" to topic \"$TOPIC_NAME\"
 kafkacat -b $KAFKA_BROKERS_SASL -P -X sasl.mechanism=PLAIN -X security.protocol=SASL_SSL -X sasl.username=token -X sasl.password=$PASSWORD -t $TOPIC_NAME -l payload.temp
 
